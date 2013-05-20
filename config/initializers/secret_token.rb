c24d5bafbcf4caa3d8c68251b151bbd58b508298
blazingpair <sibilant.threads@gmail.com>
2013-05-19 20:12:12 -0700

    list scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoList::Application.config.secret_token = '84fc80d7e227dcad91d7ebadd8b1853f19564e6c7d0baf0fc65df8d4219771d3b977854545ca878f404853bb9cdb4b91e2077a53e44716fbdab38d81cc3b2dc5'
